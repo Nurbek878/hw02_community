d7da5158af46ef7e3216bbf40724c5d776b7f1a5
Нурбек Орозалиев <nurik878878@yandex.ru>
2022-12-09 23:14:38 +0500

    Corrected recommendations for the second review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,20 +6,20 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200,
-                             verbose_name="name")
+                             verbose_name='name')
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
     class Meta:
-        verbose_name = "Group"
-        verbose_name_plural = "Groups"
+        verbose_name = 'Group'
+        verbose_name_plural = 'Groups'
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField(verbose_name="text")
+    text = models.TextField(verbose_name='text')
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
@@ -31,10 +31,10 @@ class Post(models.Model):
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='group_posts'
+        related_name='posts'
     )
 
     class Meta:
-        verbose_name = "Post"
-        verbose_name_plural = "Posts"
-        ordering = ['pub_date']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,7 @@ from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:settings.NUM_VALUES]
+    posts = Post.objects.all()[:settings.NUM_VALUES]
     context = {
         'posts': posts,
     }
@@ -14,8 +14,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.group_posts.filter(group=group).order_by(
-        '-pub_date')[:settings.NUM_VALUES]
+    posts = group.posts.filter(group=group)[:settings.NUM_VALUES]
     context = {
         'group': group,
         'posts': posts,
